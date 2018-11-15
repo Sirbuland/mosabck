c751b00dee47db29618035fd0ebb8e560b127e45
José Lourenço <joseduartelourenco@gmail.com>
2018-11-15 18:21:11 +0000

    change authentication logic: fetch user name from name payload field, search user by name and create it when it doesn't exist

--- app/components/authentication_component/interactors/authenticate_user_from_jwt.rb
+++ app/components/authentication_component/interactors/authenticate_user_from_jwt.rb
@@ -7,24 +7,19 @@ module AuthenticationComponent
         payload_info = context.jwt_payload
         msg = I18n.t('errors.messages.not_found', entity: 'Payload')
         context.fail!(message: msg) unless payload_info.present?
-        user_id = payload_info['user_id']
-        device_id = payload_info['device_id']
-        user = User.find_by(id: user_id)
+        username = payload_info['name']
+        user     = User.find_by(username: username)
         if user.present?
-          user_device = user.user_devices.find_by(device_id: device_id)
-          logged = user.own_device?(device_id) &&
-                   user_device.present? && user_device.logged_in?
-          if logged
-            context.current_user = user
-            context.current_device = user_device
-          else
-            error_msg = I18n.t('errors.messages.user_not_logged_on_device')
-            context.fail!(message: error_msg)
-          end
+          user_device = user.user_devices.first  
         else
-          error_msg = I18n.t('errors.messages.not_found', entity: 'User')
-          context.fail!(message: error_msg)
+          user = User.create(username: username)
+          user.user_devices << UserDevice.create(device_id: 123)
+
+          user_device = user.user_devices.first
         end
+
+        context.current_user = user
+        context.current_device = user_device
       end
     end
   end
