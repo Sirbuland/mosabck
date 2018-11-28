6e58ca49796d0f49ae5e2a6d0ff6c7a4c843433b
José Lourenço <joseduartelourenco@gmail.com>
2018-11-28 18:20:04 +0000

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181128152336) do
+ActiveRecord::Schema.define(version: 20181128180132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20181128152336) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["user_id"], name: "index_dashboards_on_user_id"
   end
 
@@ -123,6 +124,7 @@ ActiveRecord::Schema.define(version: 20181128152336) do
     t.bigint "dashboard_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["dashboard_id"], name: "index_panels_on_dashboard_id"
   end
 
@@ -232,6 +234,7 @@ ActiveRecord::Schema.define(version: 20181128152336) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "value"
   end
 
   add_foreign_key "auth_identities", "users"
