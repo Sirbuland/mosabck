689b2227adf28ab933085d0991ff0fb39c010a09
José Lourenço <joseduartelourenco@gmail.com>
2018-11-29 00:59:10 +0000

    add nested attributes extraction for fetchin dashboard, panels and panel vars attributes add nested save dashboard creating the dashboard and all nested panels, vars and panel vars

--- app/components/dashboard_component/interactors/extract_attributes.rb
+++ app/components/dashboard_component/interactors/extract_attributes.rb
@@ -18,20 +18,35 @@ module DashboardComponent
         end_date:    :end_date
       }.freeze
 
-      # PANEL_VAR_SCHEME = {
-
-      # }.freeze
+      VAR_SCHEME = {
+        name:  :name,
+        value: :value
+      }.freeze
 
       def call
         args = context.args
 
+        # extract dashboard attributes
         attributes        = extract_attributes(DASHBOARD_SCHEME, args)
         attributes[:user] = context.ctx[:current_user]
 
         panels = args[:panels]
         if panels
-          attributes[:panels] = panels.map do |panel_args|
-            extract_attributes(PANEL_SCHEME, panel_args)
+          attributes[:panels] = {}
+
+          # extract each panel attributes
+          panels.each_with_index do |panel_args, panel_i|
+            attributes[:panels][panel_i] = extract_attributes(PANEL_SCHEME, panel_args)
+
+            panel_vars = panel_args[:panelVars]
+            if panel_vars
+              attributes[:panels][panel_i][:panel_vars] = {}
+
+              # extract each panel var attributes
+              panel_vars.each_with_index do |panel_var_args, panel_var_i|
+                attributes[:panels][panel_i][:panel_vars][panel_var_i] = extract_attributes(VAR_SCHEME, panel_var_args)
+              end
+            end
           end
         end
 
--- app/components/dashboard_component/interactors/save_dashboard.rb
+++ app/components/dashboard_component/interactors/save_dashboard.rb
@@ -4,8 +4,36 @@ module DashboardComponent
       include Interactor
 
       def call
+        # get panels attributes
+        panels = context.attributes.delete(:panels)
+
+        # create dashboard
         dashboard = context.dashboard || Dashboard.new
         dashboard.update!(context.attributes)
+
+        panels.each do |panel_i, panel_attributes|
+          # get panel_vars attributes
+          panel_vars = panel_attributes.delete(:panel_vars)
+
+          # set panel dashboard
+          panel_attributes[:dashboard] = dashboard
+
+          # create panel
+          panel = context.panel || Panel.new
+          panel.update!(panel_attributes)
+
+          panel_vars.each do |panel_var_i, panel_var_attributes|
+            # create var
+            var = context.var || Var.new
+            var.update!(panel_var_attributes)
+
+            # create panel var relating panel and the created var
+            panel_var = context.panel_var || PanelVar.new
+            panel_var.update!(panel: panel, var: var)
+          end
+
+        end
+
         context.dashboard = dashboard
       end
     end
