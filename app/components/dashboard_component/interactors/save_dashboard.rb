f8b500259d0ef181a7124b807e3efb7751f88bed
José Lourenço <joseduartelourenco@gmail.com>
2018-11-29 18:25:25 +0000

    add ensure panels and panelVars are present before iterating them

--- app/components/dashboard_component/interactors/save_dashboard.rb
+++ app/components/dashboard_component/interactors/save_dashboard.rb
@@ -12,26 +12,30 @@ module DashboardComponent
         dashboard.update!(context.attributes)
 
         # create each panel
-        panels.each do |panel_i, panel_attributes|
-          # get panel_vars attributes
-          panel_vars = panel_attributes.delete(:panel_vars)
+        if panels
+          panels.each do |panel_i, panel_attributes|
+            # get panel_vars attributes
+            panel_vars = panel_attributes.delete(:panel_vars)
 
-          # set panel dashboard
-          panel_attributes[:dashboard] = dashboard
+            # set panel dashboard
+            panel_attributes[:dashboard] = dashboard
 
-          # create panel if it doesn't exist
-          panel = dashboard.panels[panel_i] || Panel.new
-          panel.update!(panel_attributes)
+            # create panel if it doesn't exist
+            panel = dashboard.panels[panel_i] || Panel.new
+            panel.update!(panel_attributes)
 
-          # create each panel var
-          panel_vars.each do |panel_var_i, panel_var_attributes|
+            if panel_vars
+              # create each panel var
+              panel_vars.each do |panel_var_i, panel_var_attributes|
 
-            # create var if it doesn't exist
-            var = Var.where(panel_var_attributes).first || Var.new(panel_var_attributes)
+                # create var if it doesn't exist
+                var = Var.where(panel_var_attributes).first || Var.new(panel_var_attributes)
 
-            # create panel var relating panel and the created var
-            panel_var = panel.panel_vars.where(panel: panel, var: var).first || PanelVar.new
-            panel_var.update!(panel: panel, var: var)
+                # create panel var relating panel and the created var
+                panel_var = panel.panel_vars.where(panel: panel, var: var).first || PanelVar.new
+                panel_var.update!(panel: panel, var: var)
+              end
+            end
           end
         end
 
