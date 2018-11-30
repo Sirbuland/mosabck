ff9a991f838248308d12c2c3f9edd44c7bee3a1e
José Lourenço <joseduartelourenco@gmail.com>
2018-11-30 12:15:19 +0000

    change always remove existing panels and create new ones change try to fetch existing var named  before creating a new one

--- app/components/dashboard_component/interactors/save_dashboard.rb
+++ app/components/dashboard_component/interactors/save_dashboard.rb
@@ -13,6 +13,10 @@ module DashboardComponent
 
         # create each panel
         if panels
+          
+          # delete existing panels
+          dashboard.panels.each { |panel| panel.destroy }
+          
           panels.each do |panel_i, panel_attributes|
             # get panel_vars attributes
             panel_vars = panel_attributes.delete(:panel_vars)
@@ -21,7 +25,7 @@ module DashboardComponent
             panel_attributes[:dashboard] = dashboard
 
             # create panel if it doesn't exist
-            panel = dashboard.panels[panel_i] || Panel.new
+            panel = Panel.new
             panel.update!(panel_attributes)
 
             if panel_vars
@@ -29,12 +33,11 @@ module DashboardComponent
               panel_vars.each do |panel_var_i, panel_var_attributes|
 
                 # create var if it doesn't exist
-                var = panel.vars[panel_var_i] || Var.new
+                var = Var.where(name: panel_var_attributes[:name]).first || Var.new
                 var.update(panel_var_attributes)
 
                 # create panel var relating panel and the created var
-                panel_var = panel.panel_vars.where(panel: panel, var: var).first || PanelVar.new
-                panel_var.update!(panel: panel, var: var)
+                panel_var = PanelVar.new(panel: panel, var: var).save!
               end
             end
           end
