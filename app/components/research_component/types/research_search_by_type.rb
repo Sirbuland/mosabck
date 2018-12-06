664359cc3c9cdf86515a229fcca9c1ba1a4904c0
Sirbuland <sirbuland.atlas@gmail.com>
2018-12-06 19:28:41 +0500

    add filter for research by research type

--- app/components/research_component/types/research_search_by_type.rb
+++ app/components/research_component/types/research_search_by_type.rb
@@ -1,5 +1,5 @@
 ResearchComponent::Types::ResearchSearchByType = GraphQL::EnumType.define do
   name 'ResearchSearchByType'
-  value(GraphqlHelper::RESEARCH_SEARCH_BY[:first_name], 'First name')
-  value(GraphqlHelper::RESEARCH_SEARCH_BY[:last_name], 'Last name')
+  value(GraphqlHelper::RESEARCH_SEARCH_BY[:date_authored], 'Date authored')
+  value(GraphqlHelper::RESEARCH_SEARCH_BY[:research_type], 'Reseach type')
 end
