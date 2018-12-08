6950b49e360cdd381ff58ff2737f456a11dc8bb1
Sirbuland <sirbuland.atlas@gmail.com>
2018-12-08 11:55:06 +0500

    dashboard fixes

--- db/migrate/20181208062549_change_vetted_data_type_in_exchange.rb
+++ db/migrate/20181208062549_change_vetted_data_type_in_exchange.rb
@@ -1,6 +1,11 @@
 class ChangeVettedDataTypeInExchange < ActiveRecord::Migration[5.1]
-  def change
+  def up
   	remove_column :exchanges, :vetted
   	add_column :exchanges, :vetted, :boolean
+  end
+
+  def down
+  	remove_column :exchanges, :vetted
+  	add_column :exchanges, :vetted, :string
   end
 end
