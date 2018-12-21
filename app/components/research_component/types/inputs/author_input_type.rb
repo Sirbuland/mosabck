776d7a7d2b1d81044409cf8dc9cc8a3bd5f3b694
Sirbuland <sirbuland.atlas@gmail.com>
2018-12-21 16:53:12 +0500

    fix for research authors

--- app/components/research_component/types/inputs/author_input_type.rb
+++ app/components/research_component/types/inputs/author_input_type.rb
@@ -2,7 +2,6 @@ ResearchComponent::Types::Inputs::AuthorInputType =
   GraphQL::InputObjectType.define do
     name 'AuthorInput'
 
-    argument :id, 					!types.ID
     argument :username,     types.String
     argument :description,  types.String
     argument :profession,   types.String
