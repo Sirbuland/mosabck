74f60fd875cd753be60f40b6d6519145c5d4a844
Sirbuland <sirbuland.atlas@gmail.com>
2019-01-23 13:28:13 +0500

    removed dependent destroy

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -3,4 +3,5 @@ class Keyword < ApplicationRecord
 
   has_many :keyword_research_videos
 	has_many :researches, through: :keyword_research_videos
+
 end
--- app/models/keyword_research_video.rb
+++ app/models/keyword_research_video.rb
@@ -4,4 +4,5 @@ acts_as_paranoid
   belongs_to :keyword, optional: true
   belongs_to :research, optional: true
   belongs_to :video, optional: true
+
 end
