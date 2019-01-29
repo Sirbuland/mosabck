c4f3bb84d37eec91707e425fdd9bd1595d6d1c0a
Sirbuland <sirbuland.atlas@gmail.com>
2019-01-29 14:11:10 +0500

    filters creation for users

---
+++ app/models/concerns/news_filter_creation.rb
@@ -0,0 +1,16 @@
+module NewsFilterCreation
+  extend ActiveSupport::Concern
+
+  included do
+  	after_create :create_news_filters
+  	
+  	default_filters = ["bitcoin", "ethereum", "xrp", "eos", "bitcoin", "cash", "china", "venture", "capital", "ico", "partnership"]
+  	
+  	def create_news_filters
+  		default_filters.each do |filter|
+  			news_filter = NewsFilter.find_by name: filter
+  			self.news_filters << news_filter if news_filter.present?
+  		end
+  	end
+  end
+end
