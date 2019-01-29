0196cbfed4c6cead85540ea6419e34e2392dcaec
José Lourenço <joseduartelourenco@gmail.com>
2019-01-29 20:51:57 +0000

    fix default filters

--- app/models/concerns/news_filter_creation.rb
+++ app/models/concerns/news_filter_creation.rb
@@ -6,7 +6,9 @@ module NewsFilterCreation
   	
   	default_filters = ["bitcoin", "ethereum", "xrp", "eos", "bitcoin", "cash", "china", "venture", "capital", "ico", "partnership"]
   	
-  	def create_news_filters
+		def create_news_filters
+			# quick hack: should be fixed
+			default_filters = ["bitcoin", "ethereum", "xrp", "eos", "bitcoin", "cash", "china", "venture", "capital", "ico", "partnership"]
   		default_filters.each do |filter|
   			news_filter = NewsFilter.find_by name: filter
   			self.news_filters << news_filter if news_filter.present?
