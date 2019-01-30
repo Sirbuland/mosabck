73d318708962130a1d1761d8bb3a19f34c71ec13
Sirbuland <sirbuland.atlas@gmail.com>
2019-01-30 13:18:13 +0500

    fix for login

--- app/models/concerns/news_filter_creation.rb
+++ app/models/concerns/news_filter_creation.rb
@@ -2,17 +2,16 @@ module NewsFilterCreation
   extend ActiveSupport::Concern
 
   included do
-  	after_create :create_news_filters
-  	
-  	default_filters = ["bitcoin", "ethereum", "xrp", "eos", "bitcoin", "cash", "china", "venture", "capital", "ico", "partnership"]
-  	
-		def create_news_filters
-			# quick hack: should be fixed
-			default_filters = ["bitcoin", "ethereum", "xrp", "eos", "bitcoin", "cash", "china", "venture", "capital", "ico", "partnership"]
-  		default_filters.each do |filter|
-  			news_filter = NewsFilter.find_by name: filter
-  			self.news_filters << news_filter if news_filter.present?
-  		end
+    
+    def create_news_filters
+    	unless self.news_filters.present?	
+        NewsFilter::DEFAULT_FILTERS.each do |filter|
+          news_filter = NewsFilter.find_by name: filter
+          if news_filter.present? and !self.news_filters.exists? name: news_filter.name
+      			self.news_filters << news_filter
+          end
+    		end
+      end
   	end
   end
 end
