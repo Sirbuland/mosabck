cedef2fb62ffc5a4cda66144630c7b2521c28e4e
Sirbuland <sirbuland.atlas@gmail.com>
2019-02-14 22:58:02 +0500

    slice filters hash

--- app/models/concerns/news_filter_creation.rb
+++ app/models/concerns/news_filter_creation.rb
@@ -7,13 +7,15 @@ module NewsFilterCreation
     	unless self.news_filters.present?	
         NewsFilter::DEFAULT_FILTERS.each do |filter|
           news_filter = self.news_filters.find_or_initialize_by name: filter[:name]
-          news_filter.assign_attributes({ 
-            description: filter[:description], 
-            filter_type: filter[:filter_type], 
-            search_term: filter[:search_term],
-            selected: filter[:selected],
-            priority: filter[:priority]
-          })
+          news_filter.assign_attributes(
+            filter.slice(
+              :description, 
+              :filter_type, 
+              :search_term,
+              :selected,
+              :priority
+            )
+          )
           news_filter.save!
     		end
       end
